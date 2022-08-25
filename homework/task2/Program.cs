67ae18a5b0c17a4e839615fba7469be75489b652
kaatarn-protecs <golddez@gmail.com>
2022-08-25 12:25:21 +0300

    Решение домашних заданий 2

--- homework/task2/Program.cs
+++ homework/task2/Program.cs
@@ -18,23 +18,31 @@ double k2 = Convert.ToDouble(Console.ReadLine());
 
 double[] Intersection(double b11, double k11, double b22, double k22)
 {   
-    double[] interarr = new double[2];
-    interarr[0] = (b22 - b11) / (k11 - k22); // x
-    Console.WriteLine($" x = {interarr[0]}");
-    interarr[1] = k11 * interarr[0] + b11; // y
+    double[] interarr = new double[3];
+    if (k11 == k22 && b11 == b22) interarr[2] = -2;
+    else if (k11 == k22) interarr[2] = -1;
+    else
+    {
+        interarr[0] = (b22 - b11) / (k11 - k22);
+        interarr[1] = k11 * interarr[0] + b11;
+    }
     return interarr;
 }
 void PrintArrayDouble(double[] array)
 {
     Console.Write("(");
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.Length - 1; i++)
     {
-        if (i < array.Length - 1) Console.Write($"{array[i]}; ");
+        if (i < array.Length - 2) Console.Write($"{array[i]};");
         else Console.Write(array[i]);
     }
     Console.Write(")");
 }
 
 double[] inters = Intersection(b1, k1, b2, k2);
+if (inters[2] != 0) Console.Write(inters[2] < -1 ? "<< Прямые совпадают! >>" : "<< Прямые параллельны! >>");
+else
+{
 Console.Write($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ");
 PrintArrayDouble(inters);
+}
